1612ebb0f34957abbace6e5550c32fbdafb7613d
R codes <rojagraphix0@gmail.com>
2026-08-13 15:10:07 +0300

    Add configuration file for bot settings

---
+++ config.js
@@ -0,0 +1,14 @@
+export default {
+    botName: 'ELITE-PRO-V2',
+    ownerName: 'EliteProTech',
+    owner: '256754773765',
+    prefix: ['.'],
+    botMode: 'self',
+    botMessage: {
+        owner: 'This feature is for the Owner only.',
+        admin: 'This feature is for group Admins only.',
+        group: 'This command can only be used in a group.',
+        private: 'This command can only be used in a private chat.',
+        isBotAdmin: 'I need to be an admin in this group to do that.'
+    }
+}
